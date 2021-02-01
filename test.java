54a7d363906073a709e3c7e83eaf6b9ad76c6494
Shariq <shariq7991@gmail.com>
2021-02-01 16:24:43 +0000

    Update test.java

--- test.java
+++ test.java
@@ -2,6 +2,7 @@ public class Test{
   public static void Main(String[] args){
     System.out.println("Hello World");  
     System.out.println("This is a new line of text");
+    System.out.println("This is another line");
   }
 }
 
